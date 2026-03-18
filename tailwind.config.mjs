a79d413da5313064df0516793ab29e4ed65d515c
j3rryho <61486078+j3rryho@users.noreply.github.com>
2026-03-18 14:51:35 +0800

    feat: add dark/light mode toggle

---
+++ tailwind.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: 'class',
+  content: ['./src/**/*.{astro,html,js,jsx,ts,tsx,mdx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
